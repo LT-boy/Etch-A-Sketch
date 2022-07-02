ee778620e9157d5aa4a749e0abca6525e502b911
LT-boy <aurimas.jasius@gmail.com>
2022-07-02 21:57:49 +0300

    Reset grid when slider is adjusted

--- javascript.js
+++ javascript.js
@@ -1,21 +1,22 @@
 const defaultSize = 16;
-const sizeValue = document.getElementById('sizeValue')
-const sizeSlider = document.getElementById('sizeSlider')
-const grid = document.getElementById('grid')
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
+const grid = document.getElementById('grid');
+
+let currentSize = defaultSize;
 
 function setupGrid(size) {
 	grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
 	for (let i = 0; i < size * size; i++) {
 		const gridElement = document.createElement('div');
 		gridElement.classList.add('grid-element');
-		gridElement.addEventListener('mouseover', mouseOver);
+		gridElement.addEventListener('mouseover', colourSquare);
     	grid.appendChild(gridElement);
 	}
 }
 
-function mouseOver(e) {
+function colourSquare(e) {
 	e.target.style.backgroundColor = "black";
 }
 
@@ -23,7 +24,27 @@ function updateSizeValue(value) {
 	sizeValue.innerHTML = `${value} x ${value}`;
 }
 
+function changeSize(value) {
+	setCurrentSize(value);
+	updateSizeValue(value);
+	reloadGrid();
+}
+
+function setCurrentSize(newSize) {
+	currentSize = newSize;
+}
+
+function reloadGrid() {
+	clearGrid();
+	setupGrid(currentSize);
+}
+
+function clearGrid() {
+	grid.innerHTML = '';
+}
+
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
 
 window.onload = () => {
 	setupGrid(defaultSize);
